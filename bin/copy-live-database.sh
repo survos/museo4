66a19b0241711cc18119552b81ce891141ac402c
Tac Tacelosky <tacman@gmail.com>
2019-06-12 15:09:32 -0400

    improve media player

---
+++ bin/copy-live-database.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -x
+
+echo "run the following line";
+echo "drop database news; create database news; grant all privileges on database news to news;"
+sudo -u postgres psql
+rm /tmp/latest.dump -f && heroku pg:backups:capture && heroku pg:backups:download -o /tmp/latest.dump
+pg_restore -U news -h localhost -d museo /tmp/latest.dump --no-owner
+
+# bin/create-admins.sh
+
+# sudo -u postgres pg_restore -d news -1 latest.dump --no-owner -v
+
+
+
+
---
+++ src/Migrations/Version20190612171659.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190612171659 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE exhibit ADD title VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE exhibit ADD description VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE exhibit ADD duration INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE exhibit DROP title');
+        $this->addSql('ALTER TABLE exhibit DROP description');
+        $this->addSql('ALTER TABLE exhibit DROP duration');
+    }
+}
